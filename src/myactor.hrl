9374a08e8988f62cb815a5f295fc4f23c42a65f3
Denis Justinek <yulce@yulce-macbookpro.local>
2014-01-10 11:57:15 +0100

    fix

--- src/myactor.hrl
+++ src/myactor.hrl
@@ -2,7 +2,7 @@
 % License, v. 2.0. If a copy of the MPL was not distributed with this
 % file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
--include_lib("../../actordb/src/actordb.hrl").
+-include_lib("actordb.hrl").
 
 -compile({parse_transform,lager_transform}).
 
