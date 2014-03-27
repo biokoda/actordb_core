eaf8e6e9710d618c163d9f2b41e560bc07a66dc1
Sergej Jurecko <sergej.jurecko@wpn.tv>
2014-03-27 19:00:16 +0100

    better bad commit recovery

--- src/actordb_sqlproc.erl
+++ src/actordb_sqlproc.erl
@@ -249,7 +249,7 @@ print_info(Pid) ->
 
 
 
--record(dp,{db, actorname,actortype, evnum = 0,evcrc = 0, activity = 0, timerref, start_time,
+-record(dp,{db, actorname,actortype, evnum = 0,evcrc = 0,prev_evnum = 0, prev_evcrc = 0, activity = 0, timerref, start_time,
 			page_size = 1024, activity_now,schemanum,schemavers,flags = 0,
 			% locked is a list of pids or markers that needs to be empty for actor to be unlocked.
 			locked = [],
@@ -747,6 +747,8 @@ handle_call({replicate_start,_Ref,Node,PrevEvnum,PrevCrc,Sql,EvNum,Crc,NewVers} 
 	case ok of
 		_ when Trump; P#dp.mors == slave, Node == P#dp.masternodedist, P#dp.evnum == PrevEvnum, P#dp.evcrc == PrevCrc ->
 			{reply,ok,check_timer(P#dp{replicate_sql = {Sql,EvNum,Crc,NewVers}, activity = P#dp.activity + 1})};
+		_ when P#dp.mors == slave, Node == P#dp.masternodedist, P#dp.prev_evnum == PrevEvnum, P#dp.prev_evcrc == PrevCrc ->
+			{reply,ok,check_timer(P#dp{replicate_sql = {Sql,EvNum,Crc,NewVers}, activity = P#dp.activity + 1})};
 		_ ->
 			actordb_sqlite:stop(P#dp.db),
 			case ok of
@@ -795,7 +797,7 @@ handle_call({replicate_commit,StoreLog},From,P) ->
 						 <<"$RELEASE SAVEPOINT 'adb';">>
 						 ],write),
 					{reply,okornot(Res),check_timer(wlog_after_write(StoreLog,
-									P#dp{replicate_sql = <<>>,evnum = EvNum, 
+									P#dp{replicate_sql = <<>>,evnum = EvNum, prev_evnum = P#dp.evnum,prev_evcrc = P#dp.evcrc,
 										 evcrc = Crc, activity = P#dp.activity + 1, schemavers = NewVers}))}
 			end
 	end;
@@ -806,28 +808,15 @@ handle_call({replicate_bad_commit,EvNum,Crc},_,P) ->
 	?ERR("replicate_bad_commit in ~p, my ~p",[{EvNum,Crc},{P#dp.evnum,P#dp.evcrc}]),
 	case P#dp.evnum == EvNum andalso P#dp.evcrc == Crc of
 		true ->
-			% We can find out how many pages last write contained and truncate wal file manually.
-			% After closing db handle, last write will be lost and bad commit will be forgotten.
-			ForgetWrite = fun() ->
-				wal = P#dp.journal_mode,
-				{NPrev,NPages} = actordb_sqlite:wal_pages(P#dp.db),
-				true = NPages > NPrev,
-				{ok,File} = file:open(P#dp.dbpath++"-wal",[write,read,binary,raw]),
-				{ok,_} = file:position(File,32+(P#dp.page_size+24)*NPrev),
-				ok = file:truncate(File),
-				file:close(File),
-				actordb_sqlite:stop(P#dp.db),
-				ok
-			end,
-			case catch ForgetWrite() of
+			case forget_write(P) of
 				ok ->
-					ok;
+					{reply,ok,P#dp{evcrc = P#dp.prev_evcrc, evnum = P#dp.prev_evnum}};
 				_ ->
 					actordb_sqlite:stop(P#dp.db),
-					delactorfile(P)
-			end,
-	 		{ok,NP} = init(P,replicate_bad_commit),
-	 		{reply,ok,NP#dp{callqueue = P#dp.callqueue}};
+					delactorfile(P),
+					{ok,NP} = init(P,replicate_bad_commit),
+	 				{reply,ok,NP#dp{callqueue = P#dp.callqueue}}
+			end;
 		_ ->
 			{reply,ok,P#dp{}}
 	end;
@@ -850,6 +839,26 @@ handle_call(_Msg,_From,#dp{mors = slave, verified = true} = P) ->
 handle_call(_Msg,_,P) ->
 	?AINF("sqlproc ~p unhandled call ~p mors ~p verified ~p",[P#dp.cbmod,_Msg,P#dp.mors,P#dp.verified]),
 	{reply,{error,unhandled_call},P}.
+
+
+forget_write(P) ->
+	% We can find out how many pages last write contained and truncate wal file manually.
+	% After closing db handle, last write will be lost and bad commit will be forgotten.
+	ForgetWrite = fun() ->
+		wal = P#dp.journal_mode,
+		{NPrev,NPages} = actordb_sqlite:wal_pages(P#dp.db),
+		true = NPages > NPrev,
+		{ok,File} = file:open(P#dp.dbpath++"-wal",[write,read,binary,raw]),
+		{ok,_} = file:position(File,32+(P#dp.page_size+24)*NPrev),
+		ok = file:truncate(File),
+		file:close(File),
+		actordb_sqlite:stop(P#dp.db),
+		ok
+	end,
+	case catch ForgetWrite() of
+		Res ->
+			Res
+	end.
 
 check_timer(P) ->
 	case P#dp.timerref of
